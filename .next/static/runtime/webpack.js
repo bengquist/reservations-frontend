98843813061cf21bb7fdbaebcc5df972a06bbaf3
bengquist <engquist_blake@yahoo.com>
2019-06-09 13:20:04 -0500

    fix sorter styling

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -112,7 +112,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "5a63dd600a685c487887";
+/******/ 	var hotCurrentHash = "e4cdf55802e41c130282";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- components/styles/global.ts
+++ components/styles/global.ts
@@ -40,6 +40,7 @@ export const GlobalStyle = createGlobalStyle`
   }
   body, h1, h2, h3, h4, h5, h6, p, ol, ul, input[type=text], input[type=email], button {
     margin: 0;
+    padding: 0;
     font-weight: 500;
     font-family: "pt-sans"
   }
